34dffdd605a23350fd2eca9f6afbc52f7b9e8fcf
Sam Ruby <rubys@intertwingly.net>
2017-05-25 12:01:21 +0700

    lam lai

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlowerCrown::Application.config.secret_key_base = 'fc40a871e555393d82f3cb9d396136dec04f413b17ec582a8f6d601fd1748c94f51df743cbd15686d4571cf9c7a32762bb6b71f43c78d97a6341c84b25186831'
